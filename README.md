069ecf1f8808c94db78da7061f34feb1276233cf
pat paggi <patpaggi@gmail.com>
2021-02-11 18:21:17 -0700

    routes

--- README.md
+++ README.md
@@ -16,17 +16,7 @@
 ## About the Project
 
 ```
-GIVEN a functional Express.js API
-WHEN I add my database name, MySQL username, and MySQL password to an environment variable file
-THEN I am able to connect to a database using Sequelize
-WHEN I enter schema and seed commands
-THEN a development database is created and is seeded with test data
-WHEN I enter the command to invoke the application
-THEN my server is started and the Sequelize models are synced to the MySQL database
-WHEN I open API GET routes in Insomnia Core for categories, products, or tags
-THEN the data for each of these routes is displayed in a formatted JSON
-WHEN I test API POST, PUT, and DELETE routes in Insomnia Core
-THEN I am able to successfully create, update, and delete data in my database
+
 ```
 
 ## Build With
@@ -59,5 +49,4 @@ https://p00kiebear.github.io/tech-talk/
 First clone repository!
 Then state how to test your code.
 Install node , and all the package in the dependencies.
-Next you will create and env file, then go to Mysql and install source db/schema.sql.
 Finally you will npm start.
--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -1,64 +1,20 @@
-const { User } = require('../models');
+const router = require('express').Router();
+const {
+  addTHoughts,
+  removeTHoughts,
+  addReaction,
+  removeReaction
+} = require('../../controllers/thoughts-controller');
+// to add a thought ==/api/thoughts/<userId>
+router.route('/:userId').post(addThoughts);
 
+// to add a reaction ==/api/thoughts/<userId>/<thoughtsId>
+router
+  .route('/:userId/:thoughtsId')
+  .put(addReaction)
+  .delete(removeThoughts);
 
-// the functions will go in here as methods
-const userController = {
-  getAllUser(req, res) {
-    User.find({})
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-      });
-  },
+// delete a reaction ==/api/thoughts/<userId>/<thoughtsId>/<reactionId>
+router.route('/:userId/:thoughtsId/:ReactionId').delete(removeReaction);
 
-  // get one User by id
-  getUserById({ params }, res) {
-    User.findOne({ _id: params.id })
-      .then(dbUserData => {
-        // If no User is found, send 404
-        if (!dbUserData) {
-          res.status(404).json({ message: 'No User found with this id!' });
-          return;
-        }
-        res.json(dbUserData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-      });
-  },
-
-
-  // createUser
-  createUser({ body }, res) {
-    User.create(body)
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => res.status(400).json(err));
-  },
-  // update User by id
-  updateUser({ params, body }, res) {
-    User.findOneAndUpdate({ _id: params.id }, body, { new: true })
-      .then(dbUserData => {
-        if (!dbUserData) {
-          res.status(404).json({ message: 'No User found with this id!' });
-          return;
-        }
-        res.json(dbUserData);
-      })
-      .catch(err => res.status(400).json(err));
-  },
-  // delete User
-  deleteUser({ params }, res) {
-    User.findOneAndDelete({ _id: params.id })
-      .then(dbUserData => {
-        if (!dbUserData) {
-          res.status(404).json({ message: 'No User found with this id!' });
-          return;
-        }
-        res.json(dbUserData);
-      })
-      .catch(err => res.status(400).json(err));
-  }
-}
-module.exports = userController;
+module.exports = router;
